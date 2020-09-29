6e50ff0e61a1a9085ffeef7dac367767be744212
ArloysMacias <arloys.rojas@atg.se>
2020-09-29 16:46:44 +0200

    Deleting passw

---
+++ mysql-from-python.py
@@ -0,0 +1,16 @@
+import  os
+import pymysql
+
+#Gest username from ...
+
+username =os.getenv("C9_User")
+connection=pymysql.connect(host="localhost", user="root", password="6....", db="Chinook")
+
+try:
+    with connection.cursor() as cursor:
+        sql="SELECT * FROM Artist;"
+        cursor.execute(sql)
+        result=cursor.fetchall()
+        print(result)
+finally:
+    connection.close()
